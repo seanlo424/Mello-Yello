77ff58e00cb49ece728c43d68818ce5c95a96a26
Sean O'Neil <sean.oneil424@gmail.comgit config --global user.name Sean>
2019-11-04 04:42:44 +0000

    Initial Commit

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,6 +1,9 @@
 const $setLogin = $('#login');
 const $setSignUp = $('#signup');
 const $submitButton = $('#submit');
+const $emailInput = $('#email');
+const $passwordInput = $('#password');
+const $message = $('#message');
 
 let authSetting = 'login';
 
@@ -17,3 +20,27 @@ function setAuth(setting) {
 		$submitButton.text('Sign Up');
 	}
 }
+
+function handleFormSubmit(event) {
+	event.preventDefault();
+
+	let email = $emailInput.val().trim();
+	let password = $passwordInput.val().trim();
+
+	if (!email || !password) {
+		displayMessage('Email and password fields cannot be blank.', 'danger');
+		return;
+	}
+
+	console.log(
+		`Email: ${email} Password: ${password} AuthSetting: ${authSetting}`
+		);
+}
+
+function displayMessage(message, type) {
+	$message.text(message).attr('class', type);
+}
+
+$setLogin.on('click', setAuth.bind(null, 'login'));
+$setSignUp.on('click', setAuth.bind(null, 'signup'));
+$submitButton.on('click', handleFormSubmit);
